a04bc80d36e25ddbad535eb2374cd9078fa13e32
drdude5000 <drdude5000@live.com>
2017-03-03 12:55:15 -0500

    Setup

---
+++ question.php
@@ -0,0 +1,9 @@
+<?php
+/* This php file will contain the question class. It will
+ * also contain the methods needed to score a student's response to the 
+ * question.
+ * Created By: Oscar Rodriguez
+ */
+
+
+?>
