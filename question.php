da7936d7db897702aed78fe5abd08627f52abb83
drdude5000 <drdude5000@live.com>
2017-03-04 13:40:34 -0500

    validating

--- question.php
+++ question.php
@@ -6,16 +6,55 @@
  */
 
 class question {
-	// Raw String data the question contains.
-	var $text;
+
 	// The category the question pertains to.
-	var $category;
+	public $category;
 	// The difficulty of the question  0-Easy 1-Medium 2-Hard
-	var $difficulty;
+	public $difficulty;
+	// Raw String data the question contains.
+	public $text;
 	// Sample Answer
-	var $sanswer;
-	// An array containing important words that describe the question.
-	var $dtags;
+	public $ans;
+	
+	
+	function assimilate($cat, $diff, $text){
+		$this->category = $cat;
+		$this->difficulty = $diff;
+		$this->text = $text;
+	}
+	
+	function validate_question(){
+		$floc = 'pdir/qlist.txt';
+		$qfile = fopen($floc, 'r');
+		$marr = json_decode(fread($qfile, filesize($floc)), 1);
+		
+		$check = 0;
+		$template = 0;
+		foreach($marr as $var){
+			foreach($var as $i => $j){
+				if ($i == 'category' && $j == $this->category){
+					$check = 1;
+					continue;
+				}
+				if ($i == 'difficulty' && $j == $this->difficulty && $check == 1){
+					$check = 2;
+					$template = $var;
+					break;
+				}	
+			}
+			if($check == 2)
+				break;
+		}
+		
+		if($check == 2){
+			$wquest = $var['text']
+		}
+		return False;
+	}
 }
+
+$myquestion = new question();
+$myquestion->assimilate('statement', 0, 'Print the text: [Hello Poo] as output to the terminal.');
+print_r($myquestion);
 
 ?>
