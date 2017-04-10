23abcb68102320043027f7a6de045d7764edb0e2
Oscar <drdude5000@live.com>
2017-04-10 07:26:13 +0000

    java

--- changeviewflag.php
+++ changeviewflag.php
@@ -1,12 +1,12 @@
 <?php
-$testData = array('studentName' => 'oscar');
-$backURL = "https://web.njit.edu/~em244/CS490/getTakenTests.php";
+$backURL = "https://web.njit.edu/~em244/CS490/changeExamStatus.php";
 $ch = curl_init($backURL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $testData);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
 $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
+
 ?>
