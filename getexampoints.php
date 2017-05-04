ea2270f25d447fd83126000527891af320e0fa5c
DrDude <drdude5000@live.com>
2017-05-04 18:48:16 -0400

    adding comments

--- getexampoints.php
+++ getexampoints.php
@@ -6,12 +6,10 @@ Created By: Oscar Rodriguez
 */
 
 $backURL = "http://afsaccess2.njit.edu/~em244/CS490/getScore.php";
-$jsonData = array('exam' => 'QQ');
-
 $ch = curl_init($backURL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
 $result = curl_exec($ch);
 curl_close($ch);
 
