acdcb6dc8a865ccd2882bfab829598929b524efb
DrDude <drdude5000@live.com>
2017-05-05 22:11:10 -0400

    More Java

--- sendstudentanswers.php
+++ sendstudentanswers.php
@@ -122,15 +122,15 @@ $jsonData = array(
     'points' => $pearned
 );
 
-/*
-$backURL = "http://afsaccess2.njit.edu/~em244/CS490/addGrade.php";
+
+$backURL = "http://afsaccess2.njit.edu/~em244/CS490/getPointsEarned.php";
 $ch = curl_init($backURL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
 $result = curl_exec($ch);
 curl_close($ch);
-*/
+
 
 
 /*
